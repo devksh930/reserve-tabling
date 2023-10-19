3fd574f2598b7410a50a2b362c995476526effd4
devksh930 <ksh930@icloud.com>
2023-10-20 04:00:54 +0900

    feature : spring security 설정추가

--- src/main/java/me/devksh930/tabling/app/common/config/SecurityConfig.java
+++ src/main/java/me/devksh930/tabling/app/common/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package me.devksh930.tabling.app.common.config;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,4 +19,18 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request ->
+                request.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                    .requestMatchers(HttpMethod.POST, "/accounts").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/accounts").authenticated()
+                    .anyRequest()
+                    .authenticated()
+            )
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 }
